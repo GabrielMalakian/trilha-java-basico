e3438967e8db2123ecb2679c33b2e7927fd69e5d
GabrielMalakian <gabrielaraujodeb@hotmail.com>
2026-09-03 20:57:47 -0300

    feat: finaliza o projeto ContaBanco e adiciona arquivo README

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in); 
+        
+        System.out.println("Digite o número da conta: ");
+        int numero = sc.nextInt();
+        
+        System.out.println("Digite a sua agência: ");
+        String agencia = sc.nextLine();
+        
+        sc.nextLine(); // Consumir a quebra de linha pendente
+
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = sc.next();
+        
+        sc.nextLine(); // Consumir a quebra de linha pendente
+        
+        System.out.println("Digite o seu saldo: ");
+        double saldo = sc.nextDouble();
+    
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + "R$ " + saldo + " já está disponível para saque.  ");
+
+
+
+        sc.close();
     }
 }
